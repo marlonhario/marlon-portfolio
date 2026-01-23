6f48d23d6fe705a7959685685e6da01c7f06b46e
Marlon Hario <hariomarlon83@gmail.com>
2026-01-23 11:18:25 +0800

    cleanup

--- components/WorkSliderBtns.tsx
+++ components/WorkSliderBtns.tsx
@@ -4,15 +4,7 @@ import { useSwiper } from "swiper/react";
 import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 import { Button } from "@/components/ui/button";
 
-const WorkSliderBtns = ({
-  containerStyles,
-  btnStyles,
-  iconStyles,
-}: {
-  containerStyles?: string;
-  btnStyles?: string;
-  iconStyles?: string;
-}) => {
+const WorkSliderBtns = () => {
   const swiper = useSwiper();
   return (
     <div className="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none">
